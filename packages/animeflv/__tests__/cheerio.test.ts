c96ddebb2bd4c1555f9647ea77799e875fc1f087
falsepopsky <falsepopsky@protonmail.com>
2023-06-29 18:35:45 -0300

    test: add snapshots and more fixtures

--- packages/animeflv/__tests__/cheerio.test.ts
+++ packages/animeflv/__tests__/cheerio.test.ts
@@ -2,41 +2,114 @@ import { readHTMLFile } from '../fixtures/setup.js';
 import { browse } from '../src/index.js';
 
 describe('browse()', () => {
-  /* const testCases = [
-    {
-      file: 'browse.html',
-      expectedLength: 24,
-    },
-    {
-      file: 'browse-query.html',
-      expectedLength: 24,
-    },
-    {
-      file: 'browse-query-page-10.html',
-      expectedLength: 24,
-    },
-  ];
-
-  testCases.forEach(({ file, expectedLength }) => {
-    it(`returns titles for ${file}`, async () => {
-      const browsehtml = await readHTMLFile(file);
+  describe('Fixture browse', () => {
+    it('Should return titles', async () => {
+      const browsehtml = await readHTMLFile('browse.html');
       const data = browse(browsehtml);
 
       expect(Array.isArray(data)).toBe(true);
-      expect(data).toHaveLength(expectedLength);
+      expect(data).toHaveLength(24);
+      expect(data).toMatchSnapshot();
     });
 
-    it('shows data', () => {
-      const data = browse('hey');
-      expect(data).toHaveLength(0);
+    it('Should return a string type for every value of the keys from titles', async () => {
+      const browsehtml = await readHTMLFile('browse.html');
+      const titles = browse(browsehtml);
+
+      for (const title of titles) {
+        expect(typeof title.title).toBe('string');
+        expect(typeof title.description).toBe('string');
+        expect(typeof title.slug).toBe('string');
+        expect(typeof title.type).toBe('string');
+        expect(typeof title.cover).toBe('string');
+      }
     });
-  }); */
 
-  it('returns titles', async () => {
-    const browsehtml = await readHTMLFile('browse.html');
-    const data = browse(browsehtml);
+    it('Should have the expected keys in the data object', async () => {
+      const browsehtml = await readHTMLFile('browse.html');
+      const titles = browse(browsehtml);
 
-    expect(Array.isArray(data)).toBe(true);
-    expect(data).toHaveLength(24);
+      for (const title of titles) {
+        expect(title).toHaveProperty('title');
+        expect(title).toHaveProperty('description');
+        expect(title).toHaveProperty('slug');
+        expect(title).toHaveProperty('type');
+        expect(title).toHaveProperty('cover');
+      }
+    });
+  });
+
+  describe('Fixture browse-query', () => {
+    it('Should return titles', async () => {
+      const browsehtml = await readHTMLFile('browse-query.html');
+      const data = browse(browsehtml);
+
+      expect(Array.isArray(data)).toBe(true);
+      expect(data).toHaveLength(7);
+      expect(data).toMatchSnapshot();
+    });
+
+    it('Should return a string type for every value of the keys from titles', async () => {
+      const browsehtml = await readHTMLFile('browse.html');
+      const titles = browse(browsehtml);
+
+      for (const title of titles) {
+        expect(typeof title.title).toBe('string');
+        expect(typeof title.description).toBe('string');
+        expect(typeof title.slug).toBe('string');
+        expect(typeof title.type).toBe('string');
+        expect(typeof title.cover).toBe('string');
+      }
+    });
+
+    it('Should have the expected keys in the data object', async () => {
+      const browsehtml = await readHTMLFile('browse.html');
+      const titles = browse(browsehtml);
+
+      for (const title of titles) {
+        expect(title).toHaveProperty('title');
+        expect(title).toHaveProperty('description');
+        expect(title).toHaveProperty('slug');
+        expect(title).toHaveProperty('type');
+        expect(title).toHaveProperty('cover');
+      }
+    });
+  });
+
+  describe('Fixture browse-query-page-10', () => {
+    it('Should return titles', async () => {
+      const browsehtml = await readHTMLFile('browse-query-page-10.html');
+      const data = browse(browsehtml);
+
+      expect(Array.isArray(data)).toBe(true);
+      expect(data).toHaveLength(24);
+      expect(data).toMatchSnapshot();
+    });
+
+    it('Should return a string type for every value of the keys from titles', async () => {
+      const browsehtml = await readHTMLFile('browse.html');
+      const titles = browse(browsehtml);
+
+      for (const title of titles) {
+        expect(typeof title.title).toBe('string');
+        expect(typeof title.description).toBe('string');
+        expect(typeof title.slug).toBe('string');
+        expect(typeof title.type).toBe('string');
+        expect(typeof title.cover).toBe('string');
+      }
+    });
+
+    it('Should have the expected keys in the data object', async () => {
+      const browsehtml = await readHTMLFile('browse.html');
+      const titles = browse(browsehtml);
+
+      for (const title of titles) {
+        expect(title).toHaveProperty('title');
+        expect(title).toHaveProperty('description');
+        expect(title).toHaveProperty('slug');
+        expect(title).toHaveProperty('type');
+        expect(title).toHaveProperty('cover');
+      }
+    });
   });
 });

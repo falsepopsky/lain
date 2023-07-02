d5f5dc598356fe838b8978128249443fb7fdc5de
falsepopsky <falsepopsky@protonmail.com>
2023-07-01 22:10:55 -0300

    test: add more fixtures

--- packages/animeflv/fixtures/mobile/index.ts
+++ packages/animeflv/fixtures/mobile/index.ts
@@ -1,3 +1,5 @@
 export { browse as MobileBrowse } from './browse.js';
+export { browseQuery as MobileBQ } from './browseQuery.js';
+export { browseQueryPageTen as MobileBQPT } from './browseQueryPageTen.js';
 export { fake as MobileFake } from './fake.js';
 

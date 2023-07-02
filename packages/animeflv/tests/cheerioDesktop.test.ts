6c462bd9fed0ab10a392a6a1fc99ca77cf0286db
falsepopsky <falsepopsky@protonmail.com>
2023-07-01 22:35:42 -0300

    test: rename folder, separate desktop and mobile

--- packages/animeflv/tests/cheerioDesktop.test.ts
+++ packages/animeflv/tests/cheerioDesktop.test.ts
@@ -1,5 +1,5 @@
-import { MobileBrowse, MobileFake, browse, browseQuery, browseQueryPageTen, fake } from '../fixtures/index.js';
-import { getPages, getTitles, getTitlesMobile } from '../src/index.js';
+import { browse, browseQuery, browseQueryPageTen, fake } from '../fixtures/index.js';
+import { getPages, getTitles } from '../src/index.js';
 
 describe('getTitles()', () => {
   describe('Fixture browse', () => {
@@ -147,47 +147,6 @@ describe('getPages()', () => {
       const uniquePages = Array.from(new Set(pages));
 
       expect(uniquePages).toEqual(pages);
-    });
-  });
-});
-
-describe('getTitlesMobile()', () => {
-  describe('Fixture browse', () => {
-    const titles = getTitlesMobile(MobileBrowse);
-
-    it('Should return titles', () => {
-      expect(Array.isArray(titles)).toBe(true);
-      expect(titles).toHaveLength(24);
-      expect(titles).toMatchSnapshot();
-    });
-
-    it('Should return a string type for every value of the keys from titles', () => {
-      for (const title of titles) {
-        expect(typeof title.title).toBe('string');
-        expect(typeof title.slug).toBe('string');
-        expect(typeof title.type).toBe('string');
-        expect(typeof title.cover).toBe('string');
-      }
-    });
-
-    it('Should have the expected keys in the data object', () => {
-      for (const title of titles) {
-        expect(title).toHaveProperty('title');
-        expect(title).toHaveProperty('slug');
-        expect(title).toHaveProperty('type');
-        expect(title).toHaveProperty('cover');
-      }
-    });
-  });
-
-  describe('Fixture fake', () => {
-    const titles = getTitlesMobile(MobileFake);
-
-    it('Should return empty strings for slug & cover if not found', () => {
-      // @ts-expect-error: Object is possibly 'undefined'.
-      expect(titles[0].slug).toBe('');
-      // @ts-expect-error: Object is possibly 'undefined'.
-      expect(titles[0].cover).toBe('');
     });
   });
 });

8de97e3eac29bdcb9232a7a6edd0ed15e5f6f18a
falsepopsky <falsepopsky@protonmail.com>
2023-07-03 01:50:44 -0300

    remove: animeSlug, verSlugEpisode

--- packages/animeflv/src/index.ts
+++ packages/animeflv/src/index.ts
@@ -1,2 +1,2 @@
 export { getInformationMobile, getPages, getTitles, getTitlesMobile } from './cheerio.js';
-export { animeSlug, browseAnime, browseFilter, verSlugEpisode } from './url.js';
+export { browseAnime, browseFilter } from './url.js';

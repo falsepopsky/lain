e6bf11b327809fbdff5646643892d121ab3eb25a
falsepopsky <falsepopsky@protonmail.com>
2023-07-09 21:55:49 -0300

    test: add fixtures for anime desktop path

--- packages/animeflv/fixtures/desktop/index.ts
+++ packages/animeflv/fixtures/desktop/index.ts
@@ -1,3 +1,5 @@
+export { animeEmision } from './animeEmision.js';
+export { animeFinalizado } from './animeFinalizado.js';
 export { browse } from './browse.js';
 export { browseQuery } from './browseQuery.js';
 export { browseQueryPageTen } from './browseQueryPageTen.js';
